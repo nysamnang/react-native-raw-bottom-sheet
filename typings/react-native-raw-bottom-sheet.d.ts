495e7a29558ff5f71a058a28673fb71e99692ade
gabriel.rohden <gabriel.rohden@ngi.com.br>
2019-05-24 15:42:09 -0300

    Add ts typings and bump version

---
+++ typings/react-native-raw-bottom-sheet.d.ts
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { StyleProp, ViewStyle, Animated } from 'react-native'
+
+declare module 'react-native-raw-bottom-sheet' {
+
+    type RBSheetProps = {
+        animationType?: 'none' | 'fade' | 'slide'
+        height?: number
+        minClosingHeight?: number
+        duration?: number
+        closeOnSwipeDown?: boolean
+        closeOnPressMask?:boolean
+        onClose?: () => void
+        customStyles?: {
+            wrapper?: StyleProp<ViewStyle>
+            container?: StyleProp<Animated.View>
+        }
+    }
+
+    class RBSheet extends React.Component<RBSheetProps> {
+        open(): void
+        close(): void
+    }
+
+    export default RBSheet
+}
