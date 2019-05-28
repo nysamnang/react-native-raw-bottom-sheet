3243a472e37d2ecda08c4bce1e7420edb66f0217
NY Samnang <nysamnang168@gmail.com>
2019-05-28 09:46:14 +0700

    Linting & npm audit fix

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,26 +1,25 @@
-import * as React from 'react'
-import { StyleProp, ViewStyle, Animated } from 'react-native'
+import * as React from "react";
+import { StyleProp, ViewStyle, Animated } from "react-native";
 
-declare module 'react-native-raw-bottom-sheet' {
+declare module "react-native-raw-bottom-sheet" {
+  export type RBSheetProps = {
+    animationType?: "none" | "fade" | "slide";
+    height?: number;
+    minClosingHeight?: number;
+    duration?: number;
+    closeOnSwipeDown?: boolean;
+    closeOnPressMask?: boolean;
+    onClose?: () => void;
+    customStyles?: {
+      wrapper?: StyleProp<ViewStyle>;
+      container?: StyleProp<Animated.View>;
+    };
+  };
 
-    export type RBSheetProps = {
-        animationType?: 'none' | 'fade' | 'slide'
-        height?: number
-        minClosingHeight?: number
-        duration?: number
-        closeOnSwipeDown?: boolean
-        closeOnPressMask?:boolean
-        onClose?: () => void
-        customStyles?: {
-            wrapper?: StyleProp<ViewStyle>
-            container?: StyleProp<Animated.View>
-        }
-    }
+  class RBSheet extends React.Component<RBSheetProps> {
+    open(): void;
+    close(): void;
+  }
 
-    class RBSheet extends React.Component<RBSheetProps> {
-        open(): void
-        close(): void
-    }
-
-    export default RBSheet
+  export default RBSheet;
 }
